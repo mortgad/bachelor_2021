71090818ecbec9a574b718a6a3164be5cfb6ef5d
mortgad <mortengade7400@hotmail.com>
2021-12-01 11:14:44 +0100

    Cleaning up

--- 05_plan_segment.sh
+++ 05_plan_segment.sh
@@ -2,4 +2,4 @@
 
 echo "[ ] Starting text segmenting"
 # cpu bound, so use only NCORES number of threads
-/usr/bin/find data/txt/*.txt | parallel -j 2 -X "python3 04_segment.py"
+/usr/bin/find data/txt/*.txt | parallel -j 2 -X "python3 05_segment.py"
